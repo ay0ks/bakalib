6265b2bc3a9cf0b7486008cbb468563539d11673
Dimitriy ay0ks <dimitriy.ga@gmail.com>
2022-09-12 08:19:27 +0200

    feat(input.rs): python-like input()

---
+++ src/input.rs
@@ -0,0 +1,13 @@
+use std::io;
+use std::io::*;
+
+pub fn input(prompt: &str) -> io::Result<String> {
+    print!("{}", prompt);
+    io::stdout().flush()?;
+    io::stdin()
+        .lock()
+        .lines()
+        .next()
+        .unwrap()
+        .map(|x| x.trim_end().to_owned())
+}
